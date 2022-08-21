966022d987b9a9a269a0d5225ba5dd355b72a5a1
mouayedKeziz01 <m_keziz@estin.dz>
2022-08-21 13:30:35 +0100

    add scss

--- src/info/Info.js
+++ src/info/Info.js
@@ -5,32 +5,20 @@ import mock3 from "../img/mock3.png"
 import mock4 from "../img/mock4.png"
 import mock5 from "../img/mock5.png"
 
-/* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
-how to make this template work best for you! */
 
 export let colors = ["rgb(0,255,164)", "rgb(166,104,255)"];
-/*
-I highly recommend using a gradient generator like https://gradientgenerator.paytonpierce.dev/ to generate a pair of colors that you like.
-These colors will be used to style your name on the homepage, the background of your picture, and some other accents throughout
-the site.
- */
 
 
-/*
-So let's get started! Some of the info below is pretty self-explanatory, like 'firstName' and 'bio'. I'll try to explain anything
-that might not be obvious right off the bat :) I recommend looking at the template example live using "npm start" to get an idea
-of what each of the values mean.
- */
 
 export const info = {
     firstName: "Mouayed",
     lastName: "Keziz",
-    initials: "Dev", // the example uses first and last, but feel free to use three or more if you like.
+    initials: "Dev",
     position: "a Full Stack Developer",
     selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
     gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
     baseColor: colors[0],
-    miniBio: [ // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
+    miniBio: [
         {
             emoji: '💻',
             text: 'passionate about computer science'
@@ -69,8 +57,6 @@ export const info = {
             icon: "fa fa-linkedin",
             label: 'linkedin'
         },
-        // Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
-        // Just change the links so that they lead to your social profiles.
 
     ],
     bio: "Hello! I'm Mouayed. I'm a computer science student at ESTIN, I believe artificial intelligence wont rule us all one day. You should hire me!",
@@ -94,14 +80,12 @@ export const info = {
             emoji: '🎻'
         },
 
-        // Same as above, change the emojis to match / relate to your hobbies or interests.
-        // You can also remove the emojis if you'd like, I just think they look cute :P
     ],
-    portfolio: [ // This is where your portfolio projects will be detailed
+    portfolio: [
         {
             title: "Project 1",
-            live: "https://paytonpierce.dev", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
-            source: "https://github.com/paytonjewell", // this should be a link to the **repository** of the project, where the code is hosted.
+            live: "https://paytonpierce.dev",
+            source: "https://github.com/paytonjewell",
             image: mock1
         },
         {
